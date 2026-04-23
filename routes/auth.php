8d0d46322cb780b99b8d707e0711684e9d699272
Ahmed1othman <othmanahmed865@gmail.com>
2026-04-23 18:08:31 +0200

    Fix duplication routes

--- routes/auth.php
+++ routes/auth.php
@@ -2,30 +2,35 @@
 
 declare(strict_types=1);
 
-use App\Http\Controllers\Auth\VerifyEmailController;
-use App\Livewire\Actions\Logout;
 use App\Livewire\Auth\ForgotPassword;
 use App\Livewire\Auth\Login;
 use App\Livewire\Auth\Register;
 use App\Livewire\Auth\ResetPassword;
 use App\Livewire\Auth\VerifyEmail;
 use Illuminate\Support\Facades\Route;
+use Laravel\Fortify\Features;
 
 Route::middleware('guest')->group(function () {
     Route::get('login', Login::class)->name('login');
     Route::get('register', Register::class)->name('register');
     Route::get('forgot-password', ForgotPassword::class)->name('password.request');
     Route::get('reset-password/{token}', ResetPassword::class)->name('password.reset');
+
+    if (Features::enabled(Features::twoFactorAuthentication())) {
+        Route::get('two-factor-challenge', function () {
+            if (! session('login.id')) {
+                return redirect()->route('login');
+            }
+
+            return view('livewire.auth.two-factor-challenge');
+        })->name('two-factor.login');
+    }
 });
 
 Route::middleware('auth')->group(function () {
-    // Route::get('verify-email', VerifyEmail::class)
-    //     ->name('verification.notice');
+    Route::get('verify-email', VerifyEmail::class)
+        ->name('verification.notice');
 
-    // Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
-    //     ->middleware(['signed', 'throttle:6,1'])
-    //     ->name('verification.verify');
+    Route::get('confirm-password', fn () => view('livewire.auth.confirm-password'))
+        ->name('password.confirm');
 });
-
-Route::post('logout', Logout::class)
-    ->name('logout');
